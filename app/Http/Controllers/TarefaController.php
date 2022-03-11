bd73cc1d3c7fc2c83a5e7d1f47c00ca9ad77e6aa
Leandro Santos <lds.leosantos@gmail.com>
2022-03-11 16:11:31 -0300

    Implementação Laravel Ui/ envio de emails e export para xlsx csv pdf

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class TarefaController extends Controller
+{
+
+    // public function __construct()
+    // {
+    //$this->middleware('auth');
+    // }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+
+        if (Auth::Check()) {
+            $id = auth()->user()->id;
+            $nome = Auth::user()->name;
+            $email = Auth::user()->email;
+
+            return " Id $id | Nome: $nome | Email: $email";
+        } else {
+            return 'Voce nao esta logado';
+        }
+
+        /*
+        if (auth()->check()) {
+            $id = auth()->user()->id;
+            $nome = auth()->user()->name;
+            $email = auth()->user()->email;
+
+            return " Id $id | Nome: $nome | Email: $email";
+        } else {
+            return 'Voce nao esta logado';
+        }
+        */
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}
